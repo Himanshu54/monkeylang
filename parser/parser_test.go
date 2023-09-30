fce8ea9296c163f7c0d20935290a1c6c1e583cfd
Himanshu Gusain <himu5493@gmail.com>
2023-09-30 17:05:25 +0000

    fix let statement test

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -7,38 +7,40 @@ import (
 	"testing"
 )
 
-// func TestLetStatemnts(t *testing.T) {
-// 	input := `
-// 	let x = 5;
-// 	let y = 10;
-// 	let foobar = 838383;
-// 	`
-// 	l := lexer.New(input)
-// 	p := New(l)
-//
-// 	program := p.ParseProgram()
-// 	checkParseErrors(t, p)
-// 	if program == nil {
-// 		t.Fatalf("ParseProgram() returned nil")
-// 	}
-// 	if len(program.Statements) != 6 {
-// 		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
-// 	}
-// 	test := []struct {
-// 		expectedIdentifier string
-// 	}{
-// 		{"x"},
-// 		{"y"},
-// 		{"foobar"},
-// 	}
-//
-// 	for i, tt := range test {
-// 		stmt := program.Statements[i]
-// 		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
-// 			return
-// 		}
-// 	}
-// }
+func TestLetStatemnts(t *testing.T) {
+	input := `
+	let x = 5;
+	let y = 10;
+	let foobar = 838383;
+	`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParseErrors(t, p)
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+
+	t.Logf("%v", program.Statements)
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+	}
+	test := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range test {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
 
 func checkParseErrors(t *testing.T, p *Parser) {
 	errors := p.Errors()
